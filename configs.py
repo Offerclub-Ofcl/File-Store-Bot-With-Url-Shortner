9d6f0002a1916c6ae188b6ac209f9d2dc3a1b6a2
Offerclub-Ofcl <167853706+Offerclub-Ofcl@users.noreply.github.com>
2024-05-19 21:22:27 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,17 +1,17 @@
 import os
 
 class Config(object):
-  API_ID = int(os.environ.get("API_ID", ""))
-  API_HASH = os.environ.get("API_HASH", "")
-  BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-  BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
-  DB_CHANNEL = int(os.environ.get("DB_CHANNEL", ""))
-  SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "MoneyKamalo.com")
-  SHORTLINK_API = os.environ.get('SHORTLINK_API', "0eefb93e1e3ce9470a7033115ceb1bad13a9d674")
-  BOT_OWNER = int(os.environ.get("BOT_OWNER", ""))
-  DATABASE_URL = os.environ.get("DATABASE_URL", "")
-  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
-  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+  API_ID = int(os.environ.get("API_ID", "8219941"))
+  API_HASH = os.environ.get("API_HASH", "7e264fb9a25ee1e0b33436c2119d9bc0")
+  BOT_TOKEN = os.environ.get("BOT_TOKEN", "7059504784:AAGZgySo8nFDUAcvbQwbIxOoxEFT3AIvVHU")
+  BOT_USERNAME = os.environ.get("BOT_USERNAME", "OC_FileStore_Bot")
+  DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1002065776203"))
+  SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "offerlinks.in")
+  SHORTLINK_API = os.environ.get('SHORTLINK_API', "df6b87f5c355afa37eb16f8e4b1dd46ec5975390")
+  BOT_OWNER = int(os.environ.get("BOT_OWNER", "6222756114"))
+  DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://tharunthedon001:tharunthedon001@cluster0.zody7ze.mongodb.net/")
+  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001570569040")
+  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002065776203"))
   BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
   FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
   BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
@@ -32,7 +32,7 @@ Send Me any Media or File. I can Work In Channel too. Add Me to Channel with Edi
 ╰──────[ 😎 ]───────────⍟
 """
   ABOUT_DEV_TEXT = f"""
-🧑🏻‍💻 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿: [VJ](https://telegram.me/KingVj01)
+🧑🏻‍💻 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿: [VJ](https://telegram.me/Monster_boss_0110)
  
  I am Super noob Please Support My Hard Work.
 
